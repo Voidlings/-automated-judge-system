d5a753248a255019c2c4d1f34149f1b20ca98da2
Aratakiyo <97922031+Aratakiyo@users.noreply.github.com>
2023-11-03 21:01:43 +0000

    init

---
+++ automated-judge-system/src/main/java/com/voidlings/App.java
@@ -0,0 +1,13 @@
+package com.voidlings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
