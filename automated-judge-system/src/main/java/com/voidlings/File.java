c07625b78f3343593328297ce924955a1c30216e
Aratakiyo <97922031+Aratakiyo@users.noreply.github.com>
2023-11-08 11:52:08 -0400

    file and folder

--- automated-judge-system/src/main/java/com/voidlings/File.java
+++ automated-judge-system/src/main/java/com/voidlings/File.java
@@ -1,5 +1,81 @@
 package com.voidlings;
 
-public class File{
+import java.util.ArrayList;
+import java.util.List;
 
+public class File {
+    private String name;
+    private String content;
+    private List<String> attributeList; //made string for now 
+    private List<String> methodList; // made string for now 
+
+    // Constructor
+    public File(String name, String content) {
+        this.name = name;
+        this.content = content;
+        this.attributeList = new ArrayList<>();
+        this.methodList = new ArrayList<>();
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public List<String> extractAttributes() {
+        String[] lines = content.split("\n");
+        boolean inAttributeBlock = false;
+
+        for (String line : lines) {
+            if (line.contains("{")) {
+                inAttributeBlock = true;
+            } else if ((!line.contains("{") && line.contains("}")) || (line.contains("(") && line.contains(")"))) {
+                break;
+            } else if (inAttributeBlock && !line.trim().startsWith("//") && !line.trim().startsWith("/*")) {
+                // Check for lines within the attribute block and not commented out
+                attributeList.add(line); // Extract the attribute 
+            }
+        }
+        return attributeList;
+    }
+
+    public List<String> extractMethods() {
+        methodList = new ArrayList<>();
+        String[] lines = content.split("\n");
+        boolean inMethodBlock = false;
+        int openBrackets = 0;
+        int closeBrackets = 0;
+        StringBuilder currentMethod = new StringBuilder();
+
+        for (String line : lines) {
+            if ((line.contains("(") && line.contains(")") && line.contains("{")) &&
+                (!line.trim().startsWith("//") && !line.trim().startsWith("/*"))) {
+                // Identifying the start of a method
+                openBrackets++;
+                inMethodBlock = true;
+                currentMethod.append(line).append("\n");
+            }
+
+            if (inMethodBlock) {
+                if (line.contains("{")) {
+                    openBrackets++;
+                }
+                if (line.contains("}")) {
+                    closeBrackets++;
+                }
+
+                currentMethod.append(line).append("\n");
+
+                if (openBrackets == closeBrackets) {
+                    // End of the method block
+                    methodList.add(currentMethod.toString());
+                    currentMethod.setLength(0); // Clear StringBuilder
+                    inMethodBlock = false;
+                    openBrackets = 0;
+                    closeBrackets = 0;
+                }
+            }
+        }
+        return methodList;
+    }
 }
--- automated-judge-system/src/main/java/com/voidlings/Folder.java
+++ automated-judge-system/src/main/java/com/voidlings/Folder.java
@@ -1,5 +1,42 @@
 package com.voidlings;
 
-public class Folder{
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Folder {
+    private List<File> files;
+
+    // Constructor
+    public Folder() {
+        files = new ArrayList<>();
+    }
+
+    // Method to create the actual folder
+    public void createFolder(String folderPath) {
+        File folder = new File(folderPath);
+        if (!folder.exists()) {
+            folder.mkdirs(); // Create the directory if it doesn't exist
+        }
+    }
+
+    // Method to add a file to the folder
+    public void addFile(File file) {
+        files.add(file);
+    }
+
+    // Method to check if a file exists in the folder
+    public boolean fileExists(String name) {
+        for (File file : files) {
+            if (file.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Getter method to retrieve the list of files
+    public List<File> getFiles() {
+        return files;
+    }
 }
