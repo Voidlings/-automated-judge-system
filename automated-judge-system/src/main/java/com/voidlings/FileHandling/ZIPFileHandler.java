a90199f2336c5c1a7742a8daf16a4ae3c709aeca
SarahG <97921763+SG765@users.noreply.github.com>
2023-11-14 19:15:48 -0400

    Reinsert necessary statements for file handling
    Reinserted the necessary statements for proper file handling between java vs non-java files

--- automated-judge-system/src/main/java/com/voidlings/FileHandling/ZIPFileHandler.java
+++ automated-judge-system/src/main/java/com/voidlings/FileHandling/ZIPFileHandler.java
@@ -4,11 +4,11 @@ import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-public class ZIPFileHandler implements FileHandler {
+public class ZIPFileHandler implements FileHandler{
     private Folder submissionFolder;
 
-    public ZIPFileHandler(String folderName, String path) {
-        submissionFolder = new Folder(folderName, path);
+    public ZIPFileHandler(String folderName, String path){
+        submissionFolder= new Folder(folderName, path);
     }
 
     @Override
@@ -18,18 +18,18 @@ public class ZIPFileHandler implements FileHandler {
         if (!file.exists() || !file.isFile()) {
             return false;
         }
-        // Checks if the format of the file is zip
+        //Checks if the format of the file is zip
         path = path.toLowerCase();
 
-        if (path.endsWith(".zip"))
+        if(path.endsWith(".zip"))
             return true;
-
+        
         return false;
     }
 
     @Override
     public Boolean containsFileType(String filePath) {
-        // checks if the zip contains java files
+        //checks if the zip contains java files
         try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(filePath))) {
             ZipEntry entry;
             while ((entry = zipIn.getNextEntry()) != null) {
@@ -40,10 +40,10 @@ public class ZIPFileHandler implements FileHandler {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         return false;
     }
-
+    
     @Override
     public Folder extractFiles(String zipFilePath, String destinationPath) {
         if (!containsFileType(zipFilePath)) {
@@ -62,6 +62,7 @@ public class ZIPFileHandler implements FileHandler {
             ZipEntry entry;
             while ((entry = zipIn.getNextEntry()) != null) {
                 String entryName = entry.getName();
+                File outputFile = new File(destinationPath, entryName);
 
                 if (entry.isDirectory()) {
                     // Skip directories
@@ -69,19 +70,12 @@ public class ZIPFileHandler implements FileHandler {
                     continue;
                 }
 
-             /*    if (!entryName.endsWith(".java")) {
-                    // Skip non-Java files
-                    System.out.println("Non-Java File Skipped: " + entryName);
-                    continue;
-                }
-    */
-                File outputFile = new File(destinationPath, entryName);
-
                 if (!outputFile.getParentFile().exists()) {
                     // ensure directories are made for the files
                     outputFile.getParentFile().mkdirs();
                 }
 
+                
                 // use array buffer to read entry
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 byte[] buffer = new byte[4096];
@@ -90,8 +84,17 @@ public class ZIPFileHandler implements FileHandler {
                     bos.write(buffer, 0, bytesRead);
                 }
 
-                String content = new String(bos.toByteArray());// content for SubmissionFile
+                if (!entryName.endsWith(".java")) {
+                    // if non-Java file encountered only add it to the destination directory
+                    try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                        fos.write(bos.toByteArray());
+                    }
+                }
 
+                //if java file encountered, add to the destination directory and create JavaFile instances for addition to submissionFolder
+                String content = new String(bos.toByteArray());// content for JavaFile
+
+                
                 if (entryName.contains("/")) {
                     // If the entry is in a subdirectory, create nested folders
                     String[] folders = entryName.split("/");
@@ -101,23 +104,22 @@ public class ZIPFileHandler implements FileHandler {
                         // Check if the folder already exists
                         FileComponent existingComponent = currentFolder.getComponentByName(folders[i]);
                         if (existingComponent == null) {
-                            currentFolder.addComponent(new Folder(folders[i], (destinationPath + "/" + folders[i])));
+                            currentFolder.addComponent(new Folder(folders[i], (destinationPath + "/"+ folders[i])));
                             existingComponent = currentFolder.getComponentByName(folders[i]);
                         }
                         currentFolder = (Folder) existingComponent;
                     }
-                    currentFolder.addComponent(
-                            new JavaFile(folders[folders.length - 1], content, (destinationPath + "/" + entryName)));
+                    currentFolder.addComponent(new JavaFile(folders[folders.length - 1], content, (destinationPath + "/" + entryName)));
 
                     // Write the content to the outputFile
                     try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-                        fos.write(("package com.voidlings.submissions." + currentFolder.getName() + ";\n").getBytes());
+                        fos.write(("package com.voidlings.submissions." + currentFolder.getName() + ";\n")
+                                .getBytes());
                         fos.write(bos.toByteArray());
                     }
                 } else {
                     // Otherwise, add file directly to submissionFolder
-                    submissionFolder
-                            .addComponent(new JavaFile(entryName, content, (destinationPath + "/" + entryName)));
+                    submissionFolder.addComponent(new JavaFile(entryName, content, (destinationPath + "/" + entryName)));
                 }
             }
         } catch (IOException e) {
@@ -131,5 +133,5 @@ public class ZIPFileHandler implements FileHandler {
     public Folder getFolder() {
         return this.submissionFolder;
     }
-
+    
 }
