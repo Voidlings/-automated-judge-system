511097f81eed9ae0b33d954192c7b04e6d3578b3
SarahG <97921763+SG765@users.noreply.github.com>
2023-11-16 16:17:23 +0000

    Fix App.java for zipHandling

--- automated-judge-system/src/main/java/com/voidlings/App.java
+++ automated-judge-system/src/main/java/com/voidlings/App.java
@@ -1,8 +1,9 @@
 package com.voidlings;
 
 import java.io.File;
-import com.voidlings.FileHandling.Folder;
-import com.voidlings.FileHandling.ZIPFileHandler;
+import java.util.ArrayList;
+
+import com.voidlings.FileHandling.*;
 
 public class App {
     public static void main(String[] args) {
@@ -11,47 +12,32 @@ public class App {
        com.spire.license.LicenseProvider.setLicenseKey("license.elic.xml");
        
 
+        String directoryPath= "testFiles.zip"; //replace with actual filepath name, test file for now
 
-        String directoryPath = "/workspaces/-automated-judge-system/automated-judge-system/src/main/java/com/voidlings/AssignmentSubzip";
+        String destinationPath= "submissions"; //adds to submissions folder on same directory as the App.java file. Do not change this until further notice.
+        
+        ZIPFileHandler zipFileHandler = new ZIPFileHandler(destinationPath, destinationPath);
 
-        String destinationPath = "submissions";
+        
+        // Check if the file format is a ZIP file
+        if (zipFileHandler.checkFormat(directoryPath)) {
+            System.out.println("Valid ZIP file format.");
 
-        File directory = new File(directoryPath);
-
-        if (directory.isDirectory()) {
-            File[] files = directory.listFiles();
-
-            for (File file : files) {
-                if (file.isFile() && file.getName().toLowerCase().endsWith(".zip")) {
-                    ZIPFileHandler zipFileHandler = new ZIPFileHandler(destinationPath, destinationPath);
-
-                    if (zipFileHandler.checkFormat(file.getAbsolutePath())) {
-                        System.out.println("Valid ZIP file format for file: " + file.getName());
-
-                        Folder allSubmissions = zipFileHandler.extractFiles(file.getAbsolutePath(), destinationPath);
-
-                        if ((allSubmissions != null) && (allSubmissions.getFileComponents() != null)
-                                && (allSubmissions.getFolderList() != null)) {
-                            // Uncomment to test hierarchy of folder and files
-                            /* 
-                              for (Folder folder : allSubmissions.getFolderList()) {
-                              System.out.println(folder.getName()); for(JavaFile f:
-                              folder.getJavaFiles())
-                              System.out.println(folder.getName() + ": " + f.getName()); }
-                              */
-                             
-                            System.out.println("Extraction successful for file: " + file.getName());
-                        } else {
-                            System.err.println("Extraction failed for file: " + file.getName() +
-                                    ". Ensure that the zip contains folder submissions with .java files");
-                        }
-                    } else {
-                        System.err.println("Invalid file format for file: " + file.getName() + ". ZIP File required.");
-                    }
-                }
+            // Extract files from the ZIP file
+            Folder allSubmissions = zipFileHandler.extractFiles(directoryPath, destinationPath);
+            if ((allSubmissions != null) && (allSubmissions.getFileComponents() != null) && (allSubmissions.getFolderList() != null)) {
+                // Uncomment to test heirachy of folder and files
+                /*for (Folder folder : allSubmissions.getFolderList()) {
+                        System.out.println(folder.getName());
+                    for(JavaFile f: folder.getJavaFiles())
+                        System.out.println(folder.getName() + ": " + f.getName());
+                }*/
+                System.out.println("Extraction successful.");
+            } else {
+                System.err.println("Extraction failed. Ensure that the zip contains folder submissions with .java files");
             }
         } else {
-            System.err.println("Specified path is not a directory.");
+            System.err.println("Invalid file format: ZIP File required.");
         }
     }
 }
