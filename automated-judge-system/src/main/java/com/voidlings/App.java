32c5bf0bf11359b84215d32b8dd4f17dd364bad9
Aratakiyo <97922031+Aratakiyo@users.noreply.github.com>
2023-11-20 23:28:53 -0400

    improved submission and output

--- automated-judge-system/src/main/java/com/voidlings/App.java
+++ automated-judge-system/src/main/java/com/voidlings/App.java
@@ -1,12 +1,21 @@
 package com.voidlings;
 
+import java.io.File;
 import java.util.ArrayList;
 
-import com.voidlings.FileHandling.*;
-import com.voidlings.ReportHandling.*;
-import com.voidlings.SpecificationHandling.*;
-import com.voidlings.TestCases.*;
-import com.voidlings.EvaluationHandling.*;
+import com.voidlings.EvaluationHandling.AttributeEval;
+import com.voidlings.EvaluationHandling.MethodEval;
+import com.voidlings.FileHandling.Folder;
+import com.voidlings.FileHandling.ZIPFileHandler;
+import com.voidlings.ReportHandling.EvaluationReport;
+import com.voidlings.ReportHandling.Generator;
+import com.voidlings.ReportHandling.PDFEvaluationReport;
+import com.voidlings.ReportHandling.PDFGenerator;
+import com.voidlings.SpecificationHandling.JavaHandler;
+import com.voidlings.SpecificationHandling.SpecificationComponents;
+import com.voidlings.SpecificationHandling.SpecificationHandler;
+import com.voidlings.TestCases.AttributeTestCase;
+import com.voidlings.TestCases.MethodTestCase;
 
 
 public class App {
@@ -43,74 +52,94 @@ public class App {
                 for(JavaFile f: folder.getJavaFiles())
                     System.out.println(folder.getName() + ": " + f.getName());
             }*/
-            System.out.println("Extraction successful.");
+            System.out.println("Extraction successful.\n\n");
         } else {
             System.err.println("Extraction failed. Ensure that the zip contains folder submissions with .java files");
             return;
-        }
-
-        //reads the class names,methods, and attributes, and marks for each class to an arraylist specifcation classes(specification)
-        SpecificationHandler specification = new SpecificationHandler("COMP2603  Assignment 1 Marking Scheme.pdf");
-        ArrayList<SpecificationComponents> specificationClasses = specification.readSpecificationFile();
-        /*
-        for(SpecificationComponents specClass : specificationClasses){
-            System.out.println(specClass.getClassName());
-            specClass.printAttributes();
-            specClass.printMethods();
-            specClass.printMarks();
-            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        }
-        */
+        }    
 
         for (Folder folder : allSubmissions.getFolderList()) {
-            ArrayList<String> javaFileNames= new ArrayList<>();
-            javaFileNames.addAll(folder.getJavaFilePaths()); //get all file paths in each folder
-        
-            JavaHandler javahandler = new JavaHandler(javaFileNames);
-            ArrayList<SpecificationComponents> javas = javahandler.readJava();
-            
-            /*
-            for(SpecificationComponents java: javas){
-                String name = java.getClassName();
-                //System.out.println("Class Name: " + name);
-                System.out.println("Attributes:");
-                java.printAttributes();
-                //System.out.println("Methods:");
-                //java.printMethods();
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            String specFilePath = null;
+            File directory = new File("Submissions/" + folder.getName());
+            System.out.println("Submission " + directory.getName() + " is being graded.");
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.getName().endsWith(".pdf")) {
+                        specFilePath = file.getPath();
+                        break;
+                    }
+                }
             }
-            */
 
-            // Grade each class against the specification.
-            // Attributes are graded, 1 mark each per present attribute.
-            AttributeTestCase gradeAttr = new AttributeTestCase(specificationClasses, javas);
-           
-            // Methods: Marks per method in ArrayList. If present, mark given.
-            MethodTestCase gradeMethods = new MethodTestCase(specificationClasses, javas);
-            // Sum method grades and attribute grades in order to get total grade.
-            /* 
-            System.out.println("Total marks obtained from the assignment: " + (gradeAttr.getTotalScore() + gradeMethods.getTotalScore() + " marks")); 
-            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-            */
-            ArrayList<AttributeEval> attrEval = gradeAttr.getAttributeEvals();
-            ArrayList<MethodEval> methodEval = gradeMethods.getMethodEvals();
+            if (specFilePath != null) {
+                SpecificationHandler specification = new SpecificationHandler(specFilePath);
+                ArrayList<SpecificationComponents> specificationClasses = specification.readSpecificationFile();
+                
+                System.out.println("Reading Specifiction of Assignment: " + new File(specFilePath).getName());
 
-            /* 
-            for (AttributeEval attr : attrEval){
-              System.out.println("Attibute: " + attr.name + "\nPassed: " + attr.passed + "\nScore:" + attr.score + "\n");
-              System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-            } */
+                /*
+                for(SpecificationComponents specClass : specificationClasses){
+                    System.out.println(specClass.getClassName());
+                    specClass.printAttributes();
+                    specClass.printMethods();
+                    specClass.printMarks();
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
+                */
+
+                ArrayList<String> javaFileNames= new ArrayList<>();
+                javaFileNames.addAll(folder.getJavaFilePaths()); //get all file paths in each folder
             
-            /* 
-            for (MethodEval method : methodEval){
-              System.out.println("Method: " + method.name + "\nPassed: " + method.passed + "\nScore:" + method.score + "\n");
-              System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-            } */
+                JavaHandler javahandler = new JavaHandler(javaFileNames);
+                ArrayList<SpecificationComponents> javas = javahandler.readJava();
+                
+                /*
+                for(SpecificationComponents java: javas){
+                    String name = java.getClassName();
+                    //System.out.println("Class Name: " + name);
+                    System.out.println("Attributes:");
+                    java.printAttributes();
+                    //System.out.println("Methods:");
+                    //java.printMethods();
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
+                */
+
+                // Grade each class against the specification.
+                // Attributes are graded, 1 mark each per present attribute.
+                AttributeTestCase gradeAttr = new AttributeTestCase(specificationClasses, javas);
             
-            EvaluationReport report = new PDFEvaluationReport(attrEval, methodEval);
-            Generator pdfGenerator = new PDFGenerator(report, folder.getName());
-            pdfGenerator.generate();
-            System.out.println("Report generated for " + folder.getName());
+                // Methods: Marks per method in ArrayList. If present, mark given.
+                MethodTestCase gradeMethods = new MethodTestCase(specificationClasses, javas);
+                // Sum method grades and attribute grades in order to get total grade.
+                /* 
+                System.out.println("Total marks obtained from the assignment: " + (gradeAttr.getTotalScore() + gradeMethods.getTotalScore() + " marks")); 
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                */
+                ArrayList<AttributeEval> attrEval = gradeAttr.getAttributeEvals();
+                ArrayList<MethodEval> methodEval = gradeMethods.getMethodEvals();
+
+                /* 
+                for (AttributeEval attr : attrEval){
+                System.out.println("Attibute: " + attr.name + "\nPassed: " + attr.passed + "\nScore:" + attr.score + "\n");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                } */
+                
+                /* 
+                for (MethodEval method : methodEval){
+                System.out.println("Method: " + method.name + "\nPassed: " + method.passed + "\nScore:" + method.score + "\n");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                } */
+                
+                EvaluationReport report = new PDFEvaluationReport(attrEval, methodEval);
+                Generator pdfGenerator = new PDFGenerator(report, folder.getName());
+                pdfGenerator.generate();
+                System.out.println("Report generated for " + folder.getName() + " and downloaded to Report folder.\n");
+            
+            } else {
+                System.err.println("No PDF file found in the Submission directory.");
+            }
         }
     }
 }
